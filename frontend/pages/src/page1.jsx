e6fd668ee39fb10d55c79d8a148d20325e65275e
kirubelmoges <primeforthekms@gmail.com>
2026-06-29 04:59:54 -0700

    Add pyramidal carousel depth effect with hidden descriptions on hover

--- frontend/pages/src/page1.jsx
+++ frontend/pages/src/page1.jsx
@@ -1069,7 +1069,7 @@ const App1 = () => {
                     
                     {/* Left Card - Pinned to left edge of center */}
                     {certificateCards.left && (
-                      <div className="absolute top-0 ">
+                      <div className="top-0 ">
                         <div className="transform scale-90 origin-right opacity-40 blur-[2px]">
                           <CertificateCard 
                             cert={certificateCards.left}
@@ -1084,7 +1084,7 @@ const App1 = () => {
 
                     {/* Right Card - Pinned to right edge of center */}
                     {certificateCards.right && (
-                      <div className="absolute top-0 ">
+                      <div className=" top-0 ">
                         <div className="transform scale-90 origin-left opacity-40 blur-[2px]">
                           <CertificateCard 
                             cert={certificateCards.right}
